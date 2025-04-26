09329d494a134b28dc16b44775b31021de741409
Roby Parr <roby.parr@gmail.com>
2025-04-26 23:49:06 +0900

    Update URL in eh.js

--- internal/views/static/js/lib/eh.js
+++ internal/views/static/js/lib/eh.js
@@ -1,16 +1,17 @@
 function eh(action, data) {
   var data = data || {};
 
-  var projectKey = document.querySelector('meta[name="eh_project_key"]')?.getAttribute('content');
+  var projectKey = document
+    .querySelector('meta[name="eh_project_key"]')
+    ?.getAttribute("content");
   projectKey ||= window.EH_PROJECT_KEY;
   if (!projectKey) return;
 
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://localhost:4000/api/events', true);
-  // xhr.open('POST', 'https://event-horizon.robyparr.com/api/events', true);
-  xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
-  xhr.setRequestHeader('Authorization', 'Bearer ' + projectKey);
+  xhr.open("POST", "https://eh.robyparr.com/api/events", true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  xhr.setRequestHeader("Authorization", "Bearer " + projectKey);
 
-  var jsonBody = JSON.stringify({ action: action, count: data.count || 1 })
+  var jsonBody = JSON.stringify({ action: action, count: data.count || 1 });
   xhr.send(jsonBody);
 }
