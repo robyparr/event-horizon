f6ea9e753d22900ec1f0a5938479296b2c85f83e
Roby Parr <roby.parr@gmail.com>
2025-04-30 22:10:42 +0900

    Allow customizing  library base URL with a <meta> tag

--- internal/views/static/js/lib/eh.js
+++ internal/views/static/js/lib/eh.js
@@ -1,14 +1,15 @@
 function eh(action, data) {
   var data = data || {};
 
-  var projectKey = document
-    .querySelector('meta[name="eh_project_key"]')
-    ?.getAttribute("content");
+  var projectKey = document.querySelector('meta[name="eh_project_key"]')?.getAttribute("content");
   projectKey ||= window.EH_PROJECT_KEY;
   if (!projectKey) return;
 
+  var baseURL = document.querySelector('meta[name="eh_url"]')?.getAttribute("content");
+  baseURL ||= "https://eh.robyparr.com";
+
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://eh.robyparr.com/api/events", true);
+  xhr.open("POST", baseURL + '/api/events', true);
   xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
   xhr.setRequestHeader("Authorization", "Bearer " + projectKey);
 
